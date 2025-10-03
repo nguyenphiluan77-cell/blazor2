15b5771011fe54554a747b18d354773d9d7ec690
nguyenphiluan77@gmail.com <nguyenphiluan77@gmail.com>
2025-10-03 12:14:01 +0700

    master+

---
+++ Models/productVm.cs
@@ -0,0 +1,7 @@
+public class ProductVM
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
